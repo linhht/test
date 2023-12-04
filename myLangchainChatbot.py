9f0411b8d862631fb081885e01c109e620a7d7c9
linhht <31060606+linhht@users.noreply.github.com>
2023-12-04 10:23:14 +0000

    ok

---
+++ myLangchainChatbot.py
@@ -0,0 +1,9 @@
+# Install required modules/libraries
+# pip install --upgrade pip
+# pip install --upgrade langchain
+# pip install openai
+# pip install python-dotenv
+
+# Create requirement.txt
+# pip freeze > requirements.txt
+# pip install -r requirements.txt
