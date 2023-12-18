0868c79477d76dc7fc9d2e2e164911e78e70485e
linhht <31060606+linhht@users.noreply.github.com>
2023-12-18 04:28:43 +0000

    add sum memory

--- tchat/main.py
+++ tchat/main.py
@@ -1,14 +1,43 @@
-import os
-from openai import OpenAI
-import sys
-
-# Load remote env - new way with cloud-based (dotenv.org) .env file
-from dotenv_vault import load_dotenv
-load_dotenv()
-client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
-#print(os.getenv('OPENAI_API_KEY'))
-
 # Usage:
 # alias py="DOTENV_KEY='dotenv://:key_3e01f516ab3afccab63494b905a641d5cb978d3640d6d3d6a4e29f3e76fb58ec@dotenv.org/vault/.env.vault?environment=production' python"
 # py main.py
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory, ConversationSummaryMemory
+import os
+# Load remote env - new way with cloud-based (dotenv.org) .env file
+from dotenv_vault import load_dotenv
+load_dotenv()
 
+chat = ChatOpenAI(verbose=True)
+
+memory = ConversationSummaryMemory(
+#memory = ConversationBufferMemory(
+    #chat_memory=FileChatMessageHistory("messages.json"),
+    memory_key="messages",
+    return_messages=True,
+    llm=chat
+)
+
+prompt = ChatPromptTemplate(
+    input_variables=["content", "meassages"],
+    messages=[
+        MessagesPlaceholder(variable_name="messages"),
+        HumanMessagePromptTemplate.from_template("{content}")
+    ]
+)
+
+chain = LLMChain(
+    llm=chat,
+    prompt=prompt,
+    memory=memory,
+    verbose=True
+)
+
+while True:
+    content = input(">> ")
+
+    result = chain({"content": content})
+
+    print(result["text"])
