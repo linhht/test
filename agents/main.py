9161e67f7c300a845036b24ab6e79f23a1cb13db
linhht <31060606+linhht@users.noreply.github.com>
2023-12-20 02:35:51 +0000

    test agent tools

--- agents/main.py
+++ agents/main.py
@@ -5,3 +5,37 @@
 # Load remote env - new way with cloud-based (dotenv.org) .env file
 from dotenv_vault import load_dotenv
 load_dotenv()
+
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder
+)
+from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from tools.sql import run_query_tool
+
+
+chat = ChatOpenAI()
+prompt = ChatPromptTemplate(
+    messages=[
+        HumanMessagePromptTemplate.from_template("{input}"),
+        MessagesPlaceholder(variable_name="agent_scratchpad")
+    ]
+)
+
+tools = [run_query_tool]
+
+agent = OpenAIFunctionsAgent(
+    llm=chat,
+    prompt=prompt,
+    tools=tools
+)
+
+agent_executor = AgentExecutor(
+    agent=agent,
+    verbose=True,
+    tools=tools
+)
+
+agent_executor("How many users are in the database?")
---
+++ agents/tools/sql.py
@@ -0,0 +1,19 @@
+# To support sqlite3
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import sqlite3
+from langchain.tools import Tool
+
+conn = sqlite3.connect("db.sqlite")
+
+def run_sqlite_query(query):
+    c = conn.cursor()
+    c.execute(query)
+    return c.fetchall()
+
+run_query_tool = Tool.from_function(
+    name="run_sqlite_query",
+    description="Run a sqlite query.",
+    func=run_sqlite_query
+)
