ebdb70e44e68b84022e70221fcd859b4fc4527a4
linhht <31060606+linhht@users.noreply.github.com>
2023-12-20 08:12:08 +0000

    update

--- agents/main.py
+++ agents/main.py
@@ -1,7 +1,6 @@
 # Usage:
 # alias py="DOTENV_KEY='dotenv://:key_3e01f516ab3afccab63494b905a641d5cb978d3640d6d3d6a4e29f3e76fb58ec@dotenv.org/vault/.env.vault?environment=production' python"
 # py main.py
-
 # Load remote env - new way with cloud-based (dotenv.org) .env file
 from dotenv_vault import load_dotenv
 load_dotenv()
